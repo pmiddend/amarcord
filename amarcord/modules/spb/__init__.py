d1f0fe3dc8ef2f2ec93e68c57949ec551ad7e99b
Philipp Middendorf <philipp.middendorf@desy.de>
2021-02-11 15:58:45 +0100

    Make sample changeable

--- amarcord/modules/spb/__init__.py
+++ amarcord/modules/spb/__init__.py
@@ -28,6 +28,7 @@ logger = logging.getLogger(__name__)
 class Column(Enum):
     RUN_ID = auto()
     STATUS = auto()
+    SAMPLE = auto()
     REPETITION_RATE = auto()
     TAGS = auto()
 
@@ -73,9 +74,6 @@ class _RunTables:
     table_run: sa.Table
 
 
-_db_column_to_readable: Dict[str, str] = {}
-
-
 class TagsDelegate(QtWidgets.QStyledItemDelegate):
     # pylint: disable=no-self-use
     def createEditor(
@@ -90,9 +88,6 @@ class TagsDelegate(QtWidgets.QStyledItemDelegate):
     def setEditorData(
         self, editor: QtWidgets.QWidget, index: QtCore.QModelIndex
     ) -> None:
-        logger.info(
-            "Editor data will be %s", index.model().data(index, QtCore.Qt.EditRole)
-        )
         editor.tags(index.model().data(index, QtCore.Qt.EditRole))
         editor.completion(index.model().available_tags)
 
@@ -103,6 +98,69 @@ class TagsDelegate(QtWidgets.QStyledItemDelegate):
         index: QtCore.QModelIndex,
     ) -> None:
         model.setData(index, set(editor.tagsStr()), QtCore.Qt.EditRole)
+
+
+class SampleDelegate(QtWidgets.QStyledItemDelegate):
+    # pylint: disable=no-self-use
+    def createEditor(
+        self,
+        parent: QtWidgets.QWidget,
+        option: QtWidgets.QStyleOptionViewItem,
+        index: QtCore.QModelIndex,
+    ) -> QtWidgets.QWidget:
+        result = QtWidgets.QComboBox(parent)
+        logger.info("Available samples: %s", index.model().available_samples)
+        result.insertItems(0, [str(s) for s in index.model().available_samples])
+        return result
+
+    # pylint: disable=no-self-use
+    def setEditorData(
+        self, editor: QtWidgets.QWidget, index: QtCore.QModelIndex
+    ) -> None:
+        editor.setCurrentText(index.data(QtCore.Qt.DisplayRole))
+
+    def setModelData(
+        self,
+        editor: QtWidgets.QWidget,
+        model: QtCore.QAbstractItemModel,
+        index: QtCore.QModelIndex,
+    ) -> None:
+        model.setData(index, editor.currentText(), QtCore.Qt.EditRole)
+
+
+def _retrieve_data(tables: _RunTables, conn: Any) -> List[List[Any]]:
+    run = tables.table_run
+    tag = tables.table_run_tag
+
+    select_stmt = (
+        sa.select(
+            [
+                run.c.id,
+                run.c.status,
+                run.c.sample_id,
+                run.c.repetition_rate_mhz,
+                tag.c.tag_text,
+            ]
+        )
+        .select_from(run.outerjoin(tag))
+        .order_by(run.c.id)
+    )
+    result: List[List[Any]] = []
+    for _run_id, run_rows in groupby(
+        conn.execute(select_stmt).fetchall(), lambda x: x[0]
+    ):
+        rows = list(run_rows)
+        first_row = rows[0]
+        result.append(
+            [
+                first_row[0],
+                first_row[1],
+                first_row[2],
+                first_row[3],
+                set(row[4] for row in rows if row[4] is not None),
+            ]
+        )
+    return result
 
 
 class DBModel(QtCore.QAbstractTableModel):
@@ -118,47 +176,30 @@ class DBModel(QtCore.QAbstractTableModel):
         self._dbcontext = dbcontext
 
         with self._dbcontext.connect() as conn:
-            run = self._tables.table_run
-            tag = self._tables.table_run_tag
-
             self.available_tags: List[str] = self._get_available_tags(conn)
+            self.available_samples: List[str] = self._get_available_samples(conn)
 
-            select_stmt = (
-                sa.select(
-                    [run.c.id, run.c.status, run.c.repetition_rate_mhz, tag.c.tag_text]
-                )
-                .select_from(run.outerjoin(tag))
-                .order_by(run.c.id)
-            )
-            self._data: List[List[Any]] = []
-            for _run_id, run_rows in groupby(
-                conn.execute(select_stmt).fetchall(), lambda x: x[0]
-            ):
-                rows: List[Tuple[str, float, str, str]] = list(run_rows)
-                first_row = rows[0]
-                self._data.append(
-                    [
-                        first_row[0],
-                        first_row[1],
-                        first_row[2],
-                        set(row[3] for row in rows if row[3] is not None),
-                    ]
-                )
+            self._data = _retrieve_data(tables, conn)
             self._column_to_index = {
                 Column.RUN_ID: 0,
                 Column.STATUS: 1,
-                Column.REPETITION_RATE: 2,
-                Column.TAGS: 3,
+                Column.SAMPLE: 2,
+                Column.REPETITION_RATE: 3,
+                Column.TAGS: 4,
             }
             self._index_to_column = {v: k for k, v in self._column_to_index.items()}
             self._column_headers = {
                 Column.RUN_ID: "Run",
                 Column.STATUS: "Status",
+                Column.SAMPLE: "Sample",
                 Column.REPETITION_RATE: "Repetition Rate",
                 Column.TAGS: "Tags",
             }
-            self._column_editable = set({Column.TAGS})
-            self._column_setters = {Column.TAGS: self._setTags}
+            self._column_editable = set({Column.TAGS, Column.SAMPLE})
+            self._column_setters = {
+                Column.TAGS: self._setTags,
+                Column.SAMPLE: self._setSample,
+            }
             self._column_converters: Dict[Column, Callable[[Any, int], Any]] = {
                 Column.TAGS: self._convert_tag_column
             }
@@ -168,6 +209,14 @@ class DBModel(QtCore.QAbstractTableModel):
             t[0]
             for t in conn.execute(
                 sa.select([self._tables.table_run_tag.c.tag_text]).distinct()
+            ).fetchall()
+        ]
+
+    def _get_available_samples(self, conn: Any) -> List[str]:
+        return [
+            t[0]
+            for t in conn.execute(
+                sa.select([self._tables.table_sample.c.sample_id])
             ).fetchall()
         ]
 
@@ -182,14 +231,35 @@ class DBModel(QtCore.QAbstractTableModel):
     def tags_column_index(self) -> int:
         return self._column_to_index[Column.TAGS]
 
+    def sample_column_index(self) -> int:
+        return self._column_to_index[Column.SAMPLE]
+
+    def atCell(self, row: int, column: Column) -> Any:
+        return self._data[row][self._column_to_index[column]]
+
+    def setCell(self, row: int, column: Column, value: Any) -> None:
+        self._data[row][self._column_to_index[column]] = value
+
+    def _setSample(self, row: int, value: QtCore.QVariant, role: int) -> bool:
+        if not isinstance(value, str):
+            raise Exception("didn't get a str in set sample, but " + str(type(value)))
+
+        with self._dbcontext.connect() as conn:
+            conn.execute(
+                self._tables.table_run.update()
+                .where(self._tables.table_run.c.id == self.atCell(row, Column.RUN_ID))
+                .values(sample_id=int(value))
+            )
+        self.setCell(row, Column.SAMPLE, value)
+        return True
+
     def _setTags(self, row: int, value: QtCore.QVariant, role: int) -> bool:
         if not isinstance(value, set):
             raise Exception("didn't get a list in set tags, but " + str(type(value)))
 
-        logger.info("Setting tags to %s", value)
         with self._dbcontext.connect() as conn:
             with conn.begin():
-                run_id = self._data[row][self._column_to_index[Column.RUN_ID]]
+                run_id = self.atCell(row, Column.RUN_ID)
                 run_tag = self._tables.table_run_tag
                 conn.execute(run_tag.delete().where(run_tag.c.run_id == run_id))
                 if value:
@@ -199,7 +269,7 @@ class DBModel(QtCore.QAbstractTableModel):
                     )
                 self.available_tags = self._get_available_tags(conn)
 
-        self._data[row][self._column_to_index[Column.TAGS]] = value
+        self.setCell(row, Column.TAGS, value)
         return True
 
     def rowCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
@@ -253,7 +323,7 @@ class DBModel(QtCore.QAbstractTableModel):
         )
         if column_converter is not None:
             return column_converter(v, role)
-        if role == QtCore.Qt.DisplayRole:
+        if role in (QtCore.Qt.DisplayRole, QtCore.Qt.EditRole):
             return str(v)
         return QtCore.QVariant()
 
@@ -270,6 +340,7 @@ class RunTable(QtWidgets.QWidget):
         self._context = context
         self._table_view = QtWidgets.QTableView()
         self._table_view.setAlternatingRowColors(True)
+        self._delegates: List[QtWidgets.QAbstractItemDelegate] = []
         # header = FilterHeader(self._table_view)
         # header.setFilterBoxes(self._table_model.columnCount())
         # header.filterActivated.connect(self.handleFilterActivated)
@@ -339,9 +410,16 @@ class RunTable(QtWidgets.QWidget):
         self._table_view.setModel(self._table_model)
         self._table_view.verticalHeader().hide()
         self._table_view.horizontalHeader().setStretchLastSection(True)
+        tags_delegate = TagsDelegate()
         self._table_view.setItemDelegateForColumn(
-            self._table_model.tags_column_index(), TagsDelegate()
+            self._table_model.tags_column_index(), tags_delegate
         )
+        sample_delegate = SampleDelegate()
+        self._table_view.setItemDelegateForColumn(
+            self._table_model.sample_column_index(), sample_delegate
+        )
+        # The table doesn't own the delegates, thus our storage here
+        self._delegates.extend([tags_delegate, sample_delegate])
 
     # def handleFilterActivated(self):
     #     header = self.view.horizontalHeader()
