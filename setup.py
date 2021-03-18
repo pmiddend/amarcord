733e2b838a705e32124227d9eaa08ea9bb582395
Philipp Middendorf <philipp.middendorf@desy.de>
2021-03-18 14:15:54 +0100

    amici: update to 0.2pre2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="amarcord-amici",
-    version="0.2pre1",
+    version="0.2pre2",
     author="CFEL",
     author_email="sc@cfel.de",
     description="external interface for AMARCORD",
-    packages=["amarcord"],
+    packages=["amarcord.amici"],
     install_requires=["sqlalchemy==1.3.*"],
     python_requires=">=3.8",
 )
