0183c9451cf28ef1009ff35957cbd047c638409d
Philipp Middendorf <philipp.middendorf@desy.de>
2021-03-18 13:17:43 +0100

    Webserver: more work on attributi

--- purescript-frontend/src/App/JSONSchemaType.purs
+++ purescript-frontend/src/App/JSONSchemaType.purs
@@ -4,12 +4,21 @@ import Prelude
 import App.NumericRange (NumericRange, fromMaybes)
 import Data.Argonaut (class DecodeJson, JsonDecodeError(..), decodeJson, (.:), (.:?))
 import Data.Either (Either(..))
+import Data.Lens (Lens')
+import Data.Lens.Record (prop)
 import Data.Maybe (Maybe)
+import Data.Symbol (SProxy(..))
 
 type JSONNumberData
   = { suffix :: Maybe String
     , range :: Maybe (NumericRange Number)
     }
+
+_suffix :: Lens' JSONNumberData (Maybe String)
+_suffix = prop (SProxy :: SProxy "suffix")
+
+_range :: Lens' JSONNumberData (Maybe (NumericRange Number))
+_range = prop (SProxy :: SProxy "range")
 
 data JSONSchemaType
   = JSONNumber JSONNumberData
