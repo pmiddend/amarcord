c94835d8c91d6709e7aa6cebf465eb5b99091f77
Philipp Middendorf <philipp.middendorf@desy.de>
2021-03-18 14:17:52 +0100

    setup.py: use find_packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Philipp Middendorf <philipp.middendorf@desy.de>",
     author_email="sc@cfel.de",
     description="external interface for AMARCORD",
-    packages=["amarcord.amici"],
+    packages=setuptools.find_packages(),
     install_requires=["sqlalchemy==1.3.*"],
     python_requires=">=3.8",
 )
