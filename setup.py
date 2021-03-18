3984e49af4d045acac63e8d93136bf2e9308b446
Philipp Middendorf <philipp.middendorf@desy.de>
2021-03-18 14:36:00 +0100

    amici: fix install_requires, update to 0.2pre4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="amarcord-amici",
-    version="0.2pre3",
+    version="0.2pre4",
     author="Philipp Middendorf <philipp.middendorf@desy.de>",
     author_email="sc@cfel.de",
     description="external interface for AMARCORD",
     packages=setuptools.find_packages(),
-    install_requires=["sqlalchemy==1.3.*"],
+    install_requires=["SQLAlchemy==1.3.23"],
     python_requires=">=3.8",
 )
