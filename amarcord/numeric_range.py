126356405d904ef539af4a52963fb7b50aa8db51
Philipp Middendorf <philipp.middendorf@desy.de>
2021-03-22 15:40:38 +0100

    Numeric range: add function to generate random range

--- amarcord/numeric_range.py
+++ amarcord/numeric_range.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from typing import Optional
 
@@ -26,6 +27,18 @@ class NumericRange:
         ):
             return False
         return True
+
+
+def random_from_range(r: NumericRange) -> float:
+    left = r.minimum if r.minimum is not None else r.minimum_inclusive
+    right = r.maximum if r.maximum is not None else r.maximum_inclusive
+    if left is not None and right is not None:
+        return random.uniform(left, right)
+    if left is None and right is None:
+        return random.uniform(-10000, 10000)
+    if left is not None and right is None:
+        return random.uniform(left, left + 10000)
+    return random.uniform(right - 10000, right)
 
 
 def empty_range() -> NumericRange:
