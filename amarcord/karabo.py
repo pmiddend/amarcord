6c499187fa837b4d7013cbd436dcea48fa91d14b
Luca Gelisio <luca.gelisio@cfel.de>
2021-04-05 22:54:22 +0200

    To handle the Karabo stream

---
+++ amarcord/karabo.py
@@ -0,0 +1,10 @@
+from typing import Dict
+
+
+def navigate(stream: Dict):
+
+    for ki, vi in data.items():
+        print("{}".format(ki))
+
+        for kj, vj in vi.items():
+            print("  {} {}".format(kj, vj))
