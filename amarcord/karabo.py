6e94a9cd430f12ce1ffc8a67f5c2a8ab810cea03
Luca Gelisio <luca.gelisio@cfel.de>
2021-04-07 17:29:13 +0200

    Skeleton of some functions

--- amarcord/karabo.py
+++ amarcord/karabo.py
@@ -1,10 +1,46 @@
-from typing import Dict
+from typing import Any, Dict
+import os
+import yaml
 
 
-def navigate(stream: Dict):
+def load_configuration(descriptor: str):
+    # should AMARCORD send the configuration to the daemon?
 
-    for ki, vi in data.items():
+    if os.path.exists(descriptor):
+        with open(descriptor) as fh:
+            configuration = yaml.load(fh, Loader=yaml.Loader)
+
+    else:
+        raise FileNotFoundError("{} not found...".format(descriptor))
+
+    return configuration
+
+
+def parse_configuration(stream: Dict[str, Any]):
+    # remember to apply default values
+
+    # run information
+    if "run" in stream:
+        pass
+
+    else:
+        raise ValueError("Run configuration missing")
+
+    return
+
+
+def navigate(stream: Dict[str, Any]):
+    """Navigate the stream from the Karabo bridge
+
+    Args:
+        stream (Dict[str, Any]): [description]
+    """
+
+    for ki, vi in stream.items():
         print("{}".format(ki))
 
         for kj, vj in vi.items():
             print("  {} {}".format(kj, vj))
+
+
+# at the first run check there are extra entries in the stream
