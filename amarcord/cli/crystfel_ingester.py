1d599a029933ca0d2ce5add2cde9ffb61dac493a
Philipp Middendorf <philipp.middendorf@desy.de>
2021-04-20 08:41:03 +0200

    CrystFEL: injester, first version

---
+++ amarcord/cli/crystfel_ingester.py
@@ -0,0 +1,98 @@
+import logging
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+from amarcord.amici.crystfel.injest import harvest_folder
+from amarcord.amici.crystfel.injest import ingest_data_source
+from amarcord.db.db import DB
+from amarcord.db.proposal_id import ProposalId
+from amarcord.db.tables import create_tables
+from amarcord.modules.dbcontext import CreationMode
+from amarcord.modules.dbcontext import DBContext
+
+logging.basicConfig(
+    format="%(asctime)-15s %(levelname)s %(message)s", level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> int:
+    parser = ArgumentParser(description="Ingest indexing data for AMARCORD")
+
+    parser.add_argument(
+        metavar="folder",
+        nargs="*",
+        type=str,
+        default=".",
+        help="Folder to process",
+        dest="folder",
+    )
+    parser.add_argument(
+        "--stream-pattern",
+        type=str,
+        default="*.stream*",
+        help="Pattern for matching stream filenames",
+    )
+    parser.add_argument(
+        "--harvest-filename",
+        type=str,
+        default="parameters.json",
+        help="Name of harvest file",
+    )
+    parser.add_argument(
+        "--connection-url", type=str, help="Database URL", required=True
+    )
+    parser.add_argument(
+        "--proposal-id",
+        type=int,
+        help="Proposal ID to search for analysis results in",
+    )
+    parser.add_argument("--run-id", type=int, help="Run ID", required=True)
+    parser.add_argument(
+        "--force-create-run",
+        action="store_true",
+        help="Should we create runs that don't exist?",
+        required=False,
+    )
+    args = parser.parse_args()
+    dbcontext = DBContext(args.connection_url)
+    tables = create_tables(dbcontext)
+    if args.connection_url.startswith("sqlite://"):
+        dbcontext.create_all(creation_mode=CreationMode.CHECK_FIRST)
+    db = DB(dbcontext, tables)
+
+    if args.connection_url.startswith("sqlite://"):
+        # Just for testing!
+        with db.dbcontext.connect() as local_conn:
+            if not db.have_proposals(local_conn):
+                db.add_proposal(local_conn, args.proposal_id)
+    for folder in args.folder:
+        with db.connect() as conn:
+            with conn.begin():
+                run_ids = set(db.retrieve_run_ids(conn, args.proposal_id))
+                updated_data_sources = harvest_folder(
+                    db.retrieve_analysis_data_sources(conn),
+                    Path(folder),
+                    args.run_id,
+                    args.stream_pattern,
+                    args.harvest_filename,
+                )
+                for ds in updated_data_sources:
+                    run_ids = ingest_data_source(
+                        db,
+                        conn,
+                        args.force_create_run,
+                        ProposalId(args.proposal_id),
+                        run_ids,
+                        ds,
+                    )
+                logger.info(f"updated data sources: {updated_data_sources}")
+
+    # noinspection PyUnreachableCode
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ tests/indexamajig/test_crystfel.py
@@ -0,0 +1,91 @@
+import gzip
+from pathlib import Path
+from shutil import copyfile
+from shutil import copyfileobj
+
+from amarcord.amici.crystfel.injest import harvest_folder
+from amarcord.db.table_classes import DBDataSource
+from amarcord.db.table_classes import DBHitFindingResult
+from amarcord.db.table_classes import DBLinkedDataSource
+from amarcord.db.table_classes import DBLinkedHitFindingResult
+
+
+def test_harvest_folder_without_cxi(tmp_path) -> None:
+    copyfile(Path(__file__).parent / "parameters.json", tmp_path / "parameters.json")
+
+    with (Path(__file__).parent / "test001.stream.gz").open("rb") as read_file:
+        with gzip.open(read_file) as read_file_gzip:
+            with (tmp_path / "test001.stream").open("wb") as write_file:
+                copyfileobj(read_file_gzip, write_file)
+
+    resulting_ds = harvest_folder([], tmp_path, 1, "*stream*", "parameters.json")
+
+    assert len(resulting_ds) == 1
+
+    ds = resulting_ds[0]
+    assert ds.data_source.run_id == 1
+    assert ds.data_source.number_of_frames > 0
+    assert len(ds.hit_finding_results) == 1
+    hfr = ds.hit_finding_results[0]
+    assert len(hfr.indexing_results) == 1
+
+
+def test_harvest_folder_cxi(tmp_path) -> None:
+    copyfile(
+        Path(__file__).parent / "parameters-from-cxi.json", tmp_path / "parameters.json"
+    )
+
+    with (Path(__file__).parent / "test001.stream.gz").open("rb") as read_file:
+        with gzip.open(read_file) as read_file_gzip:
+            with (tmp_path / "test001.stream").open("wb") as write_file:
+                copyfileobj(read_file_gzip, write_file)
+
+    resulting_ds = harvest_folder([], tmp_path, 1, "*stream*", "parameters.json")
+
+    assert len(resulting_ds) == 0
+
+    resulting_ds = harvest_folder(
+        [
+            DBLinkedDataSource(
+                DBDataSource(
+                    id=1,
+                    run_id=1,
+                    number_of_frames=1,
+                    source={},
+                    tag=None,
+                    comment=None,
+                ),
+                hit_finding_results=[
+                    DBLinkedHitFindingResult(
+                        hit_finding_result=DBHitFindingResult(
+                            id=1,
+                            peak_search_parameters_id=1,
+                            hit_finding_parameters_id=1,
+                            data_source_id=1,
+                            result_filename="/run/media/taw/Data SSD 2020/2021/CPV/cxis0613-r0003.cxi",
+                            result_type="stream",
+                            average_peaks_event=None,
+                            average_resolution=None,
+                            number_of_hits=1,
+                            hit_rate=1,
+                            tag=None,
+                            comment=None,
+                        ),
+                        # Not needed for this test, we just want to check if the cxi file is really compared and the
+                        # hit finding result found
+                        peak_search_parameters=None,  # type: ignore
+                        hit_finding_parameters=None,  # type: ignore
+                        indexing_results=[],
+                    )
+                ],
+            )
+        ],
+        tmp_path,
+        1,
+        "*stream*",
+        "parameters.json",
+    )
+
+    assert resulting_ds
+    assert resulting_ds[0].hit_finding_results
+    assert resulting_ds[0].hit_finding_results[0].indexing_results
