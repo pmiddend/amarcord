6d54eb633e2807580bcc4ee569825818fd31427c
Philipp Middendorf <philipp.middendorf@desy.de>
2021-11-18 16:03:21 +0100

    constants: don’t blame Luca, blame Jerome!

--- amarcord/db/constants.py
+++ amarcord/db/constants.py
@@ -8,6 +8,6 @@ ATTRIBUTO_NAME_REGEX = "[a-z][a-z0-9_]*"
 
 CONTACT_INFO: Final = (
     '<ul><li>Philipp Middendorf <a href="mailto:philipp.middendorf@desy.de">philipp.middendorf@desy'
-    '.de</a></li><li>Luca Gelisio <a href="mailto:luca.gelisio@desy.de">luca.gelisio@desy.de</a></li><li><a '
+    '.de</a></li><li>Jerome Carnis <a href="mailto:jerome.carnis@desy.de">jerome.carnis@desy.de</a></li><li><a '
     'href="https://cfel-rocketchat.desy.de/group/amarcord">AMARCORD on RocketChat</a></ul> '
 )
