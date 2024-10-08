62b5c55dd71d05d5d5b006ed7a6b67683a20e21d
Philipp Middendorf <philipp.middendorf@desy.de>
2024-10-08 07:38:25 +0200

    Resolve "🐛 Fix GitLab code coverage report"

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -5,5 +5,10 @@ set -o pipefail
 
 # Taken liberally from
 # https://docs.gitlab.com/ee/ci/testing/test_coverage_visualization.html#python-example
-pytest --cov=amarcord/ --cov-branch --cov-report xml:coverage.xml --junitxml=report.xml tests
+# and
+# https://docs.gitlab.com/ee/ci/testing/test_coverage_visualization/cobertura.html#python-example
+#
+# Note that --cov-report term is necessary, because GitLab CI parses this output to
+# determine the percentage values.
+pytest --cov=amarcord/ --cov-branch --cov-report term --cov-report xml:coverage.xml --junitxml=report.xml tests
 coverage xml
