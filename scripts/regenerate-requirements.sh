f9ce0612e55d4a439568978f32227ef47a40fbec
Philipp Middendorf <philipp.middendorf@desy.de>
2025-01-13 09:11:08 +0100

    Resolve "🐛 ruff: split imports like isort did"

--- scripts/regenerate-requirements.sh
+++ scripts/regenerate-requirements.sh
@@ -4,6 +4,7 @@ set -eu
 set -o pipefail
 
 echo "Regenerating requirements.txt using uv."
-uv pip compile pyproject.toml -o requirements.txt
+uv pip compile pyproject.toml -o requirements-new.txt
+mv requirements-new.txt requirements.txt
 echo "Done, requirements.txt updated!"
 
